e8f06cb1bc2d32617c1c5fadcf2ee459d35ce708
Anastazja <61288356+AnaKr77@users.noreply.github.com>
2025-04-06 11:56:19 +0200

    Create orzechy.cpp

---
+++ orzechy.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <set>
+#include <iomanip> 
+using namespace std;
+double a,b;
+
+int main()
+{
+    cin>>a>>b;
+    cout << fixed << setprecision(3) << a/b;
+    
+
+    return 0;
+}
